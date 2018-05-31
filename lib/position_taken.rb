230809d0d3221a3a78e22a94bc6ff29ec822fd93
Kristen Papke <klpapke215@gmail.com>
2018-05-31 18:23:32 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,9 @@ def position_taken?(board, index)
     false
   elsif board[index] == ""
     false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || "O"
+    true
     end
 end
